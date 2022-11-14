2334524e672438320ab1a8ca5bc75f67962c2496
Aleksey <alekseymamaev99@mail.ru>
2022-11-15 00:00:32 +0300

    Выполнил итоговую проверочную работу!

--- Program.cs
+++ Program.cs
@@ -3,16 +3,17 @@
 Console.WriteLine("Введите количество элементов массивва : ");
 int n = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[n]; // начальный массив (введеный пользователем)
-string[] massiv = new string[n];
+string[] massiv = new string[n];// создание "конечного" массива
 
 for (int i = 0; i < array.Length; i++)
 {
     Console.WriteLine("Введите элемент массива №" + (i + 1));
     array[i] = Console.ReadLine(); // Заполнение массива (начального)
-    if (array[i].Length < 4)
-        massiv[i] = array[i];
+    if (array[i].Length < 4)       // Проверкаа условия 
+        massiv[i] = array[i];      // заполнение конечного массива при выполнении условия
 
 }
+// Далее оформление вывода 
 Console.Write("Начальный массив:");
 Console.Write($"[ " + String.Join("; ", array) + " ]");
 Console.WriteLine();
