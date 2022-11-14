9dafcc2b6dfac2f98a418681b006e6590a86cd4d
Aleksey <alekseymamaev99@mail.ru>
2022-11-14 22:29:20 +0300

    Написал программу

--- Program.cs
+++ Program.cs
@@ -2,14 +2,30 @@
 
 Console.WriteLine("Введите количество элементов массивва : ");
 int n = Convert.ToInt32(Console.ReadLine());
-string[] array = new string[n];
+string[] array = new string[n]; // начальный массив (введеный пользователем)
+string[] massiv = new string[n];
 
 for (int i = 0; i < array.Length; i++)
 {
     Console.WriteLine("Введите элемент массива №" + (i + 1));
-    array[i] = Convert.ToString(Console.ReadLine());    
+    array[i] = Console.ReadLine(); // Заполнение массива (начального)
+    if (array[i].Length < 4)
+        massiv[i] = array[i];
+
 }
-Console.Write("Изначальный массив:");
+Console.Write("Начальный массив:");
 Console.Write($"[ " + String.Join("; ", array) + " ]");
+Console.WriteLine();
+Console.Write("Отобранный массив:");
+Console.Write("[");
+for (int i = 0; i < n; i++)
+{
+    if (massiv[i] != null)
+    {
+        Console.Write(massiv[i] + ';');
+    }
+    
+}
+Console.Write("]");
 
 
