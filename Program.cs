6565412a4c93ba7696b0c88d3702295461f8e9bd
Aleksey <alekseymamaev99@mail.ru>
2022-11-14 18:42:42 +0300

    Изначальный массив

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите количество элементов массивва : ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите элемент массива №" + (i + 1));
+    array[i] = Convert.ToString(Console.ReadLine());    
+}
+Console.Write("Изначальный массив:");
+Console.Write($"[ " + String.Join("; ", array) + " ]");
+
+
